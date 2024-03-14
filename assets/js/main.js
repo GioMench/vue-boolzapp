d0650376f4cb0b2a968c27c94672f65c5ca907c6
GioMench <giorgiaglm@gmail.com>
2024-03-14 14:55:48 +0100

    milestone 2 create function for active contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,70 @@
 const { createApp } = Vue
 
-  createApp({
+createApp({
     data() {
-      return {
-        contacts: [
-            {
-                image: 'avatar_1.jpg',
-                name: 'Michele',
-                lastMessage: 'Lorem ipsum dolor sit amet'
-            },{
-                image: 'avatar_2.jpg',
-                name: 'Fabio',
-                lastMessage: 'Lorem ipsum dolor sit amet'
-            },{
-                image: 'avatar_3.jpg',
-                name: 'Samuele',
-                lastMessage: 'Lorem ipsum dolor sit amet'
-            },{
-                image: 'avatar_4.jpg',
-                name: 'Alesandro B.',
-                lastMessage: 'Lorem ipsum dolor sit amet'
-            },{
-                image: 'avatar_5.jpg',
-                name: 'Alessandro L.',
-                lastMessage: 'Lorem ipsum dolor sit amet'
-            },{
-                image: 'avatar_6.jpg',
-                name: 'Alessandra',
-                lastMessage: 'Lorem ipsum dolor sit amet'
-            },{
-                image: 'avatar_7.jpg',
-                name: 'Marco',
-                lastMessage: 'Lorem ipsum dolor sit amet'
-            },{
-                image: 'avatar_8.jpg',
-                name: 'Luca',
-                lastMessage: 'Lorem ipsum dolor sit amet'
-            }
-        ]
-        
-      }
+        return {
+            activeContact : 0,
+            contacts: [
+                {
+                    image: 'avatar_1.jpg',
+                    name: 'Michele',
+                    lastMessage: 'Lorem ipsum dolor sit amet',
+                    myMessage:' My message with Michele',
+                    contactMessage: 'Michele answer'
+                }, {
+                    image: 'avatar_2.jpg',
+                    name: 'Fabio',
+                    lastMessage: 'Lorem ipsum dolor sit amet',
+                    myMessage:' My message with Fabio',
+                    contactMessage: 'Fabio answer'
+                }, {
+                    image: 'avatar_3.jpg',
+                    name: 'Samuele',
+                    lastMessage: 'Lorem ipsum dolor sit amet',
+                    myMessage:' My message with Samuele',
+                    contactMessage: 'Samuele answer'
+                }, {
+                    image: 'avatar_4.jpg',
+                    name: 'Alesandro B.',
+                    lastMessage: 'Lorem ipsum dolor sit amet',
+                    myMessage:' My message with Ale B.',
+                    contactMessage: 'Ale B. answer'
+                }, {
+                    image: 'avatar_5.jpg',
+                    name: 'Alessandro L.',
+                    lastMessage: 'Lorem ipsum dolor sit amet',
+                    myMessage:' My message with Ale L.',
+                    contactMessage: 'Ale L. answer'
+                }, {
+                    image: 'avatar_6.jpg',
+                    name: 'Alessandra',
+                    lastMessage: 'Lorem ipsum dolor sit amet',
+                    myMessage:' My message with Ale',
+                    contactMessage: 'Ale answer'
+                }, {
+                    image: 'avatar_7.jpg',
+                    name: 'Marco',
+                    lastMessage: 'Lorem ipsum dolor sit amet',
+                    myMessage:' My message with Marco',
+                    contactMessage: 'Marco answer'
+                }, {
+                    image: 'avatar_8.jpg',
+                    name: 'Luca',
+                    lastMessage: 'Lorem ipsum dolor sit amet',
+                    myMessage:' My message with Luca',
+                    contactMessage: 'Luca answer'
+                }
+            ]
+
+        }
+    },
+
+    methods: {
+        activeTheContact(){
+            document.getElementsByClassName('card').addEventListener('click',function(){
+                this.contact = this.activeContact
+            })
+        }
+
     }
-  }).mount('#app')
+}).mount('#app')
--- readme.md
+++ readme.md
@@ -7,3 +7,11 @@ dall’interlocutore (bianco) assegnando due classi CSS diverse
 nome e immagine di ogni contatto.
     - creo una array di oggetti in cui ogni oggetto avrà come proprieta image, nome e anteprima ultimo messaggio (orario?),
     - tramite il v-for lo stampo nella dom 
+
+# Milestone 2
+● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
+messaggi relativi al contatto attivo all’interno del pannello della conversazione.
+
+    - aggiungo i messaggi all'interno di ogni oggetto contact 
+    
+● Click sul contatto mostra la conversazione del contatto cliccato
