1d323871851296ccc1525e4ffeb4d93d26ac1996
GioMench <giorgiaglm@gmail.com>
2024-03-14 17:39:25 +0100

    add active contanct image and nakìme in info-chaton

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,67 +3,197 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            activeContact : 0,
+            activeContact: 0,
             contacts: [
                 {
                     image: 'avatar_1.jpg',
                     name: 'Michele',
                     lastMessage: 'Lorem ipsum dolor sit amet',
-                    myMessage:' My message with Michele',
-                    contactMessage: 'Michele answer'
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'My message with Michele',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'My message with Michele',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Michele answer',
+                            status: 'received'
+
+                        }
+                    ]
                 }, {
                     image: 'avatar_2.jpg',
                     name: 'Fabio',
                     lastMessage: 'Lorem ipsum dolor sit amet',
-                    myMessage:' My message with Fabio',
-                    contactMessage: 'Fabio answer'
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'My message with Fabio',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'My message with Fabio',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Fabio answer',
+                            status: 'received'
+
+                        }
+                    ]
                 }, {
                     image: 'avatar_3.jpg',
                     name: 'Samuele',
                     lastMessage: 'Lorem ipsum dolor sit amet',
-                    myMessage:' My message with Samuele',
-                    contactMessage: 'Samuele answer'
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'My message with Samuele',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'My message with Samuele',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Samuele answer',
+                            status: 'received'
+
+                        }
+                    ]
                 }, {
                     image: 'avatar_4.jpg',
                     name: 'Alesandro B.',
                     lastMessage: 'Lorem ipsum dolor sit amet',
-                    myMessage:' My message with Ale B.',
-                    contactMessage: 'Ale B. answer'
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'My message with Ale B.',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'My message with Ale B.',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Ale B. answer',
+                            status: 'received'
+
+                        }
+                    ]
                 }, {
                     image: 'avatar_5.jpg',
                     name: 'Alessandro L.',
                     lastMessage: 'Lorem ipsum dolor sit amet',
-                    myMessage:' My message with Ale L.',
-                    contactMessage: 'Ale L. answer'
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'My message with Ale L.',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'My message with Ale L.',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Ale L. answer',
+                            status: 'received'
+
+                        }
+                    ]
+                    
+                    
                 }, {
                     image: 'avatar_6.jpg',
                     name: 'Alessandra',
                     lastMessage: 'Lorem ipsum dolor sit amet',
-                    myMessage:' My message with Ale',
-                    contactMessage: 'Ale answer'
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'My message with Ale',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'My message with Ale',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Ale answer',
+                            status: 'received'
+
+                        }
+                    ]
                 }, {
                     image: 'avatar_7.jpg',
                     name: 'Marco',
                     lastMessage: 'Lorem ipsum dolor sit amet',
-                    myMessage:' My message with Marco',
-                    contactMessage: 'Marco answer'
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'My message with Marco',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'My message with Marco',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Marco answer',
+                            status: 'received'
+
+                        }
+                    ]
                 }, {
                     image: 'avatar_8.jpg',
                     name: 'Luca',
                     lastMessage: 'Lorem ipsum dolor sit amet',
-                    myMessage:' My message with Luca',
-                    contactMessage: 'Luca answer'
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'My message with Luca',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'My message with Luca',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Luca answer',
+                            status: 'received'
+
+                        }
+                    ]
                 }
             ],
 
-           
+
         }
- 
+
     },
 
     methods: {
-        activeTheContact(index){
-            this.activeContact = index 
+        activeTheContact(index) {
+            this.activeContact = index
         }
 
     }
